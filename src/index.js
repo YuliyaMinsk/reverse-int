c97ef697f8dcba1183530b1722d9d623c3d46f3a
Yuliya Niaverava <79609660+YuliyaMinsk@users.noreply.github.com>
2021-07-21 21:56:58 +0300

    fix: add processing for zero

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-  if ( n > 0) {
+  if ( n >= 0) {
     return String(n).split('').reverse().join('');
   } else {
     return String(n).split('').reverse().slice(0, -1).join('');

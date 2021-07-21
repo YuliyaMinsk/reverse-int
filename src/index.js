bbb94d705c1e3f591ea1c4770e460e6da42f8e9b
Yuliya <yuliya.rostova@gmail.com>
2021-07-21 21:50:13 +0300

    feat: add solution to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  if ( n > 0) {
+    return String(n).split('').reverse().join('');
+  } else {
+    return String(n).split('').reverse().slice(0, -1).join('');
+  }
 }
